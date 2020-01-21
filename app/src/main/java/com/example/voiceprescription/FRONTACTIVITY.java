e09844e6b292f37b90d27f947004cc287cfd2c53
Neelabhra Bhattacharyya <neelabhra08@gmail.com>
2020-01-21 14:39:11 +0530

    Second Page completed

--- app/src/main/java/com/example/voiceprescription/FRONTACTIVITY.java
+++ app/src/main/java/com/example/voiceprescription/FRONTACTIVITY.java
@@ -3,12 +3,43 @@ package com.example.voiceprescription;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class FRONTACTIVITY extends AppCompatActivity {
+public class FRONTACTIVITY extends AppCompatActivity implements AdapterView.OnItemSelectedListener
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frontactivity);
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this, R.array.Gender,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text=parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+    public void text1(View view)
+    {
+       EditText name =(EditText) findViewById(R.id.editText);
+        EditText age =(EditText) findViewById(R.id.editText1);
+        Spinner gen = (Spinner) findViewById(R.id.spinner);
+
+
     }
 }
